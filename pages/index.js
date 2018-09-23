3f8613d8791d9a1b0c1c25e9aeb1200f768b03a3
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-09-23 17:32:52 +0900

    Dynamic Pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
+import Link from "next/link";
 import withLayout from "../lib/withLayout";
+import PostLink from "../components/PostLink";
 
-const Index = () => <h1>Index</h1>;
+const Index = () => (
+  <div>
+    <h1>Index</h1>
+    <ul>
+      <li>
+        <PostLink title={"Hi!"} />
+      </li>
+      <li>
+        <PostLink title={"How are you"} />
+      </li>
+    </ul>
+  </div>
+);
 
 export default withLayout(Index);
