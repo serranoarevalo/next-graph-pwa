cc55d7f9cd014289079ce3f8fa71444639a32310
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-09-23 18:40:46 +0900

    Fetching Data with getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,28 @@
-const Index = () => (
+import axios from "axios";
+import Link from "next/link";
+
+const Index = props => (
   <div>
     <h1>Movies</h1>
-    <ul />
+    <ul>
+      {props.movies.map(movie => (
+        <li key={movie.id}>
+          <Link href={`/movie?id=${movie.id}`} as={`/movie/${movie.id}`}>
+            <a>{movie.title}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
   </div>
 );
+
+Index.getInitialProps = async () => {
+  const {
+    data: { data }
+  } = await axios("https://yts.am/api/v2/list_movies.json");
+  return {
+    movies: data.movies
+  };
+};
 
 export default Index;
--- pages/movie.js
+++ pages/movie.js
@@ -1,3 +1,25 @@
-const About = () => <h1>This a movie</h1>;
+import axios from "axios";
+import { withRouter } from "next/router";
 
-export default About;
+const About = ({ movie }) => (
+  <div>
+    <h1>
+      {movie.title} | {movie.year}
+    </h1>
+    <p>{movie.description_intro}</p>
+  </div>
+);
+
+About.getInitialProps = async context => {
+  const {
+    query: { id }
+  } = context;
+  const { data } = await axios(
+    `https://yts.am/api/v2/movie_details.json?movie_id=${id}`
+  );
+  return {
+    movie: data.data.movie
+  };
+};
+
+export default withRouter(About);
